c27de1947bcea182591c81f24b8102e38002a4c8
Phong Ly <lyquocphong@gmail.com>
2023-05-01 00:22:56 +0300

    Add more environment variable into project

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,77 @@
+import { Environment } from "src/common/constants";
+
 export default () => ({
-    app_port: process.env.APP_PORT || 3000,
-    app_name: process.env.APP_NAME || 'Munchi Api',
-    ordering_co_webhook_secret: process.env.ORDERING_CO_WEBHOOK_SECRET || ''
+    APP_PORT: process.env.APP_PORT,
+    APP_NAME: process.env.APP_NAME,
+    ORDERING_CO_WEBHOOK_SECRET: process.env.ORDERING_CO_WEBHOOK_SECRET,
+    WOLT_API_ENDPOINT: getWoltApiEndpoint(),
+    WOLT_KEY: getWoltKey(),
+    WOLT_MERCHANT_ID: getWoltMerchantId(),
+    WOLT_VENUE_ID: null,
+    WOLT_WEBHOOK_SECRET: getWoltWebhookSecret(),
+    WOLT_WEBHOOK_CALLBACK_URL: getWoltWebhookCallback(),
+    HERE_GEOCODING_API_ENDPOINT: process.env.HERE_GEOCODING_API_ENDPOINT,
+    HERE_APP_ID: process.env.HERE_APP_ID,
+    HERE_API_KEY: process.env.HERE_API_KEY,
+    ORDERING_APP_ENDPOINT: process.env.ORDERING_APP_ENDPOINT,
+    ORDERING_APP_API_KEY: process.env.ORDERING_APP_API_KEY,
+    ORDERING_DELIVERY_COMPANY_API_KEY:
+        process.env.ORDERING_DELIVERY_COMPANY_API_KEY,
+    ORDERING_DELIVERY_COMPANY_NAME:
+        process.env.ORDERING_DELIVERY_COMPANY_NAME,
+    TELEGRAM_BOT_KEY: process.env.TELEGRAM_BOT_KEY,
 });
+
+function getWoltEnvironment() {
+    return process.env.NODE_ENV || Environment.Development;
+}
+
+function getWoltApiEndpoint() {
+    const woltEnv = getWoltEnvironment();
+
+    if (woltEnv == Environment.Development) {
+        return process.env.WOLT_API_DEVELOPMENT_ENDPOINT;
+    }
+
+    return process.env.WOLT_API_PRODUCTION_ENDPOINT;
+}
+
+function getWoltWebhookCallback() {
+    const woltEnv = getWoltEnvironment();
+
+    if (woltEnv == Environment.Development) {
+        return process.env.WOLT_WEBHOOK_CALLBACK_URL_DEVELOPMENT;
+    }
+
+    return process.env.WOLT_WEBHOOK_CALLBACK_URL_PRODUCTION;
+}
+
+function getWoltWebhookSecret() {
+    const woltEnv = getWoltEnvironment();
+
+    if (woltEnv == Environment.Development) {
+        return process.env.WOLT_WEBHOOK_SECRET_DEVELOPMENT;
+    }
+
+    return process.env.WOLT_WEBHOOK_SECRET_PRODUCTION;
+}
+
+function getWoltMerchantId() {
+    const woltEnv = getWoltEnvironment();
+
+    if (woltEnv == Environment.Development) {
+        return process.env.WOLT_MERCHANT_ID_DEVELOPMENT;
+    }
+
+    return process.env.WOLT_MERCHANT_ID_PRODUCTION;
+}
+
+function getWoltKey() {
+    const woltEnv = getWoltEnvironment();
+
+    if (woltEnv == Environment.Development) {
+        return process.env.WOLT_KEY_DEVELOPMENT;
+    }
+
+    return process.env.WOLT_KEY_PRODUCTION;
+}
--- src/config/validation.ts
+++ src/config/validation.ts
@@ -1,44 +1,49 @@
 import { plainToInstance } from 'class-transformer';
 import { IsEnum, IsNumber, IsString, validateSync } from 'class-validator';
-
-enum Environment {
-  Development = "development",
-  Production = "production",
-  Test = "test",
-  Provision = "provision",
-}
+import { Environment } from 'src/common/constants';
 
 class EnvironmentVariables {
-  //@IsEnum(Environment)
-  //NODE_ENV: Environment;
+    @IsEnum(Environment)
+    NODE_ENV: Environment;
 
-  @IsNumber()
-  APP_PORT: number;
+    @IsNumber()
+    APP_PORT: number;
 
-  @IsString()
-  APP_NAME: string;
+    @IsString()
+    APP_NAME: string;
 
-  @IsString()
-  ORDERING_CO_WEBHOOK_SECRET: string;
+    @IsString()
+    DATABASE_URL: string;
 
-  @IsString()
-  DATABASE_URL: string;
+    @IsString()
+    SHADOW_DATABASE_URL: string;    
 
-  @IsString()
-  SHADOW_DATABASE_URL: string;
+    @IsString()
+    ORDERING_CO_WEBHOOK_SECRET: string;
 
+    @IsString()
+    ORDERING_APP_ENDPOINT: string;
+
+    @IsString()
+    ORDERING_APP_API_KEY: string;
+
+    @IsString()
+    ORDERING_DELIVERY_COMPANY_API_KEY: string;
+
+    @IsString()
+    ORDERING_DELIVERY_COMPANY_NAME: string;
 }
 
 export function validate(config: Record<string, unknown>) {
-  const validatedConfig = plainToInstance(
-    EnvironmentVariables,
-    config,
-    { enableImplicitConversion: true },
-  );
-  const errors = validateSync(validatedConfig, { skipMissingProperties: false });
+    const validatedConfig = plainToInstance(
+        EnvironmentVariables,
+        config,
+        { enableImplicitConversion: true },
+    );
+    const errors = validateSync(validatedConfig, { skipMissingProperties: false });
 
-  if (errors.length > 0) {
-    throw new Error(errors.toString());
-  }
-  return validatedConfig;
+    if (errors.length > 0) {
+        throw new Error(errors.toString());
+    }
+    return validatedConfig;
 }
