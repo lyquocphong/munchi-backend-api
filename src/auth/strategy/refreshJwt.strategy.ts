e05aa00398028904195d06cba334efd3eb7f773e
quochuy278 <quochuy270800@gmail.com>
2023-05-01 23:28:56 +0300

    fix environment variables follow the new config

--- src/auth/strategy/refreshJwt.strategy.ts
+++ src/auth/strategy/refreshJwt.strategy.ts
@@ -1,14 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { Request } from "express";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
 @Injectable()
 export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
-    constructor() {
+    constructor(configService: ConfigService) {
       super({
         jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-        secretOrKey: process.env.JWT_REFRESH_SECRET,
+        secretOrKey: configService.get('app.jwt_refresh_secret'),
         passReqToCallback: true,
       });
     }
