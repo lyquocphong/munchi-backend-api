e94aca316d1adde9d50ab0e395061a84ceeb43f6
quochuy278 <quochuy270800@gmail.com>
2023-05-01 12:30:43 +0300

    add ordering sign in response dto

---
+++ src/ordering-co/dto/ordering-co.dto.ts
@@ -0,0 +1,28 @@
+import { Expose, Type } from 'class-transformer';
+
+export class OrderingSignInResponseDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  name: string;
+  @Expose()
+  lastName: string;
+
+  @Expose()
+  email: string;
+
+  @Expose()
+  level: number;
+
+  @Expose()
+  accessToken: string;
+
+  
+  @Expose()
+  tokenType: string;
+
+  
+  @Expose()
+  expireIn: number;
+}
