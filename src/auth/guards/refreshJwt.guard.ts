b501aadfd35fd47619e845f4e86f2b6b81c16b91
quochuy278 <quochuy270800@gmail.com>
2023-05-01 23:29:26 +0300

    change name to refreshJwtGuard

--- src/auth/guards/refreshJwt.guard.ts
+++ src/auth/guards/refreshJwt.guard.ts
@@ -1,6 +1,6 @@
 import { AuthGuard } from "@nestjs/passport";
 
-export class RefreshJwt extends AuthGuard('jwt-refresh') {
+export class RefreshJwtGuard extends AuthGuard('jwt-refresh') {
     constructor() {
       super();
     }
